8ff65ea38db848488e3494bffad7b9ecd16367f8
pancham01 <pancham9557@gmail.com>
2025-08-24 12:26:42 +0530

    update

--- src/main/java/com/mvc/controller/ElectronicsController.java
+++ src/main/java/com/mvc/controller/ElectronicsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/electronics")
-public class WelcomeController {
+public class ElectronicsController {
 
 	@RequestMapping("/")
 	public String greet() {
--- src/main/java/com/mvc/controller/FootwearController.java
+++ src/main/java/com/mvc/controller/FootwearController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/footwear")
-public class EmployeeController {
+public class FootwearController {
 	
 	
 	@RequestMapping("/")
